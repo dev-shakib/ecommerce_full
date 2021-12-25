5f8f4ae08c2e43455fca75f641ba2dc98d2b54d1
dev-shakib <h@lskit.com>
2021-12-25 13:49:30 +0600

    home page new refactored

--- Themes/Storefront/views/public/home/index.blade.php
+++ Themes/Storefront/views/public/home/index.blade.php
@@ -22,30 +22,31 @@
     @endif
 
 
-    @if (setting('storefront_product_tabs_2_section_enabled'))
-    <product-tabs-two :data="{{ json_encode($tabProductsTwo) }}"></product-tabs-two>
+    @if (setting('storefront_product_tabs_1_section_enabled'))
+        <product-tabs-one :data="{{ json_encode($productTabsOne) }}"></product-tabs-one>
     @endif
-
 
 
     @if (setting('storefront_one_column_banner_enabled'))
         <banner-one-column :banner="{{ json_encode($oneColumnBanner) }}"></banner-one-column>
     @endif
 
-    @if (setting('storefront_flash_sale_and_vertical_products_section_enabled'))
-        <flash-sale-and-vertical-products :data="{{ json_encode($flashSaleAndVerticalProducts) }}"></flash-sale-and-vertical-products>
-    @endif
 
     @if (setting('storefront_product_grid_section_enabled'))
         <product-grid :data="{{ json_encode($productGrid) }}"></product-grid>
     @endif
 
-    @if (setting('storefront_product_tabs_1_section_enabled'))
-        <product-tabs-one :data="{{ json_encode($productTabsOne) }}"></product-tabs-one>
+
+    @if (setting('storefront_product_tabs_2_section_enabled'))
+    <product-tabs-two :data="{{ json_encode($tabProductsTwo) }}"></product-tabs-two>
     @endif
 
     @if (setting('storefront_two_column_banners_enabled'))
         <banner-two-column :data="{{ json_encode($twoColumnBanners) }}"></banner-two-column>
+    @endif
+
+    @if (setting('storefront_flash_sale_and_vertical_products_section_enabled'))
+        <flash-sale-and-vertical-products :data="{{ json_encode($flashSaleAndVerticalProducts) }}"></flash-sale-and-vertical-products>
     @endif
 
     @if (setting('storefront_top_brands_section_enabled') && $topBrands->isNotEmpty())
