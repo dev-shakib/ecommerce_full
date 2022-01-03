48a898548e101567cdf1c5c72d5c1bbdc885c14d
shakil-lskit <sakil@lskit.com>
2022-01-03 12:39:58 +0600

    currection on category api route file

--- Modules/Category/Routes/public.php
+++ Modules/Category/Routes/public.php
@@ -6,6 +6,3 @@ Route::get('categories', 'CategoryController@index')->name('categories.index');
 
 Route::get('categories/{category}/products', 'CategoryProductController@index')->name('categories.products.index');
 
-Route::prefix('api')->group(function () {
-    Route::get('categories', 'Api\CategoryApiController@index');
-});
