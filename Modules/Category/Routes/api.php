abb04acebc444233801ca46509da8afc7be4ff38
nsakib9 <sakil@lskit.com>
2022-01-09 20:11:50 +0600

    show product list by category

--- Modules/Category/Routes/api.php
+++ Modules/Category/Routes/api.php
@@ -2,4 +2,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('categories', 'CategoryApiController@index');
+Route::get('categories/{category}/products', 'CategoryProductController@index');
 Route::get('featured-categories', 'CategoryApiController@featuredCategories');
+
+
