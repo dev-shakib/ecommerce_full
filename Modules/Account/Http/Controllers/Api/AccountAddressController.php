ddf4fc3bec0d8494e0131a9d866d82b0e4a96478
nsakib9 <sakil@lskit.com>
2022-01-15 13:20:08 +0600

    some changes on address controller

--- Modules/Account/Http/Controllers/Api/AccountAddressController.php
+++ Modules/Account/Http/Controllers/Api/AccountAddressController.php
@@ -20,21 +20,14 @@ class AccountAddressController extends Controller
     {
         $countries = Country::supported();
         $state = State::get(key($countries));
-
+        $userAddress = Address::where('user_id', auth('api')->user()->id)->first() ? Address::where('user_id', auth('api')->user()->id)->first(): '';
         return response([
+            'address' => $userAddress,
             'countries' => $countries,
             'state' => $state
         ]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     * @return Response
-     */
-    public function create()
-    {
-        return view('user::create');
-    }
 
     /**
      * Store a newly created resource in storage.
@@ -82,44 +75,5 @@ class AccountAddressController extends Controller
 
     }
 
-    /**
-     * Show the specified resource.
-     * @param int $id
-     * @return Response
-     */
-    public function show($id)
-    {
-        return view('user::show');
-    }
 
-    /**
-     * Show the form for editing the specified resource.
-     * @param int $id
-     * @return Response
-     */
-    public function edit($id)
-    {
-        return view('user::edit');
-    }
-
-    /**
-     * Update the specified resource in storage.
-     * @param Request $request
-     * @param int $id
-     * @return Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     * @param int $id
-     * @return Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
 }
