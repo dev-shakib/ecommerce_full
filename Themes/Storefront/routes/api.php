3dbf9d97aac5fbd83de54422dd03ab2fc6a64e31
nsakib9 <sakil@lskit.com>
2022-01-19 12:10:03 +0600

    feature api added

--- Themes/Storefront/routes/api.php
+++ Themes/Storefront/routes/api.php
@@ -1,7 +1,11 @@
 <?php
 use Illuminate\Support\Facades\Route;
+use Themes\Storefront\Feature;
 
 Route::get('storefront/tab-products/sections/{sectionNumber}/tabs/{tabNumber}', 'TabProductController@index');
 Route::get('storefront/product-grid/tabs/{tabNumber}', 'ProductGridController@index');
 Route::get('storefront/vertical-products/{columnNumber}', 'VerticalProductController@index');
+Route::get('storefront/features', function(){
+    return Feature::all()->take(4);
+});
 // Route::get('storefront/flash-sale-products', 'FlashSaleProductController@index')->name('storefront.flash_sale_products.index');
