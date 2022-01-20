1e5c10110e4e607d1a4b90cb88bfa202f7b7a22e
nsakib9 <sakil@lskit.com>
2022-01-20 16:54:57 +0600

    new changes

--- public/index.php
+++ public/index.php
@@ -1,5 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: http://localhost:8080");
+header("Access-Control-Allow-Origin: https://galaxy-app.hasib.dev");
 //header("Access-Control-Allow-Methods", "DELETE, POST, GET, OPTIONS");
 header('Access-Control-Allow-Headers: Content-Type');
 header("Access-Control-Allow-Credentials:true");
