0899eb22e9759a985db0b5cf4249e5d200574802
nsakib9 <sakil@lskit.com>
2022-01-20 16:56:09 +0600

    changes

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php
 header("Access-Control-Allow-Origin: https://galaxy-app.hasib.dev");
 //header("Access-Control-Allow-Methods", "DELETE, POST, GET, OPTIONS");
-header('Access-Control-Allow-Headers: Content-Type');
+header('Access-Control-Allow-Headers: *');
 header("Access-Control-Allow-Credentials:true");
 
 if ($_SERVER['REQUEST_METHOD'] == "OPTIONS") {//send back preflight request response
