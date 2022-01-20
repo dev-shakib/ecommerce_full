d802bbca6117e74b20064e307209509d5c493385
nsakib9 <sakil@lskit.com>
2022-01-20 16:32:31 +0600

    new fix

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php
-header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: http://localhost:8080");
 //header("Access-Control-Allow-Methods", "DELETE, POST, GET, OPTIONS");
-header("Access-Control-Allow-Headers:*");
+header('Access-Control-Allow-Headers: Content-Type');
 header("Access-Control-Allow-Credentials:true");
 
 if ($_SERVER['REQUEST_METHOD'] == "OPTIONS") {//send back preflight request response
